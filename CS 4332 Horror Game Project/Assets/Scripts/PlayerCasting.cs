04a0f223daedec2f9f934bd1c2544288880bd2fc
DirtBag537 <67401816+DirtBag537@users.noreply.github.com>
2021-05-09 20:47:03 -0500

    Stalker AI and player HP
    Modified scene to allow for stalking and added the Stalker AI to chase the player and attack them

--- CS 4332 Horror Game Project/Assets/Scripts/PlayerCasting.cs
+++ CS 4332 Horror Game Project/Assets/Scripts/PlayerCasting.cs
@@ -6,14 +6,74 @@ public class PlayerCasting : MonoBehaviour
 {
     public static float DistanceFromTarget;
     public float ToTarget;
-
+    public int playerHealth;
+    public bool runDamage;
+    float damageTime;
+    float currentDamageTime;
+    public bool attackProc;
+    public GameObject monsterDist;
+    float attackRange;
+    float currentTime;
+    //public float invulnTime = 3.0f;
+    //private bool invulnPhasee = false;
     // Update is called once per frame
+
+    void Start()
+    {
+        playerHealth = 5;
+        runDamage = false;
+        attackRange = 3f;
+        damageTime = 5.0f;
+        currentTime = 0;
+    }
     void Update()
     {
         RaycastHit Hit;
-        if(Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward),out Hit)) {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit))
+        {
             ToTarget = Hit.distance;
             DistanceFromTarget = ToTarget;
         }
+
+        if (playerHealth == 0)
+        {
+            //transform.position = new Vector3(1.5f, 2.25f, 0.0f);
+        }
+        if (playerHealth > 0)
+            currentTime += Time.deltaTime;
+        if ((Vector3.Distance(transform.position, monsterDist.transform.position) < attackRange))
+        {
+
+            if (currentTime >= damageTime)
+            {
+                Damage();
+            }
+
+        }
+
     }
+
+    public void Damage()
+    {
+        playerHealth -= 1;
+        currentTime = 0;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("The player has entered the enemies trigger");
+
+        if (other.gameObject.CompareTag("Monster")) ;
+        {
+            //Debug.Log("We have entered the damage");
+            currentDamageTime += Time.deltaTime;
+            if (currentDamageTime > damageTime)
+            {
+                playerHealth -= 1;
+                currentDamageTime = 0.0f;
+            }
+        }
+    }
+
 }
---
+++ CS 4332 Horror Game Project/Assets/Scripts/StalkerAI.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StalkerAI : MonoBehaviour
+{
+    public GameObject stalkerDest;
+    NavMeshAgent stalkerAgent;
+    public GameObject monster;
+    public static bool isStalking;
+    private bool canAttack = true;
+    public float enemyCooldown = 1;
+    private float nextSwipeTime;
+    Animator monsterAnimator;
+    public PlayerCasting player;
+    bool m_Attack;
+    void Start()
+    {
+        stalkerAgent = GetComponent<NavMeshAgent>();
+        monsterAnimator = monster.GetComponent<Animator>();
+        m_Attack = false;
+        isStalking = true;
+    }
+
+    void Update()
+    {
+        float attackRange = 3f;
+        float swingRate = 1.5f;
+        if (isStalking == false)
+        {
+            monster.GetComponent<Animator>().Play("Idle");
+        }
+        else if (Vector3.Distance(transform.position, stalkerDest.transform.position) < attackRange && canAttack)
+        {
+            if (Time.time > nextSwipeTime)
+            {
+                stalkerAgent.isStopped = true;
+                stalkerAgent.velocity = Vector3.zero;
+                monsterAnimator.SetBool("Attack", true);
+                //Target within attack range
+                //monster.GetComponent<Animator>().Play("Mutant Swiping");
+                Debug.Log("Attack should of finished at: " + Time.time);
+                //stalkerDest.runDamage = true;
+                nextSwipeTime = Time.time + swingRate;
+            }
+
+        }
+        else {
+            if (Time.time > nextSwipeTime)
+            {
+                stalkerAgent.isStopped = false;
+                monsterAnimator.SetBool("Attack", false);
+                monster.GetComponent<Animator>().Play("Crouched Walking 1");
+                stalkerAgent.SetDestination(stalkerDest.transform.position);
+            }
+        }
+
+
+
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        Debug.Log("Started Attack Cooldown at:" + Time.time);
+        canAttack = false;
+        yield return new WaitForSeconds(5);
+        canAttack = true;
+        Debug.Log("Finished waiting at: " + Time.time);
+    }
+
+
+}
